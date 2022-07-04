4651a8ac20444611bc6afeee2d2fe2cf9f41e4e8
Joel Flores <j_flores88@outlook.com>
2022-07-03 21:10:34 -0700

    All tests passing for Ship factory

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,20 +1,33 @@
 export const Ship = (name, length, startCoord, vertical = false) => {
+    const shipCoords = [];
 
-    const shipCoord = (startCoord) => {
-        const shipCoord = [];
+    const setCoords = (startCoord) => {
         if(vertical) {
             for(let i = 0; i < length; i++) {
-                shipCoord.push(startCoord + i * 10)
+                shipCoords.push(startCoord + i * 10);
             } 
         } else {
             for(let i = 0; i < length; i++) {
-                shipCoord.push(startCoord + i)
+                shipCoords.push(startCoord + i);
             }
         }
-        return shipCoord
+        return shipCoords;
     }
 
+    const hit = (coord) => {
+        for(let [index, coords] of shipCoords.entries()) {
+            if(coord === coords) {
+                shipCoords[index] = 'x';
+            }
+        }
+        return shipCoords;
+    };
+
+    const isSunk = () => shipCoords.every(e => e === 'x');
+
+    setCoords(startCoord);
+
     return {
-        name, length, startCoord, vertical, shipCoord
+        name, length, startCoord, vertical, shipCoords, setCoords, hit, isSunk
     }
 };
--- src/factories/tests/ship.test.js
+++ src/factories/tests/ship.test.js
@@ -1,11 +1,29 @@
-import { Ship } from '../ship.js'
+import { Ship } from '../ship.js';
+const carrier = Ship('carrier', 4, 2);
+const sub = Ship('sub', 3, 10, true);
 
 it('Places ships horizontally', () => {
-    const carrier = Ship('carrier', 4, 2);
-    expect(carrier.shipCoord(2)).toStrictEqual([2, 3, 4, 5])
+    expect(carrier.shipCoords).toStrictEqual([2, 3, 4, 5])
 });
 
 it('Places ships vertically', () => {
-    const carrier = Ship('carrier', 4, 2, true);
-    expect(carrier.shipCoord(2)).toStrictEqual([2, 12, 22, 32])
+    expect(sub.shipCoords).toStrictEqual([10, 20, 30])
+});
+
+it('Sets length of ship', () =>{
+    expect(sub.length).toBe(3);
+});
+
+//Hit function
+it('Marks correct targets with an "x"', () => {
+    sub.hit(20);
+    expect(sub.shipCoords).toStrictEqual([10, "x", 30]);
+});
+
+//isSunk Function
+it('Returns true if all coords are marked "x"', () => {
+    sub.hit(10);
+    sub.hit(20);
+    sub.hit(30);
+    expect(sub.isSunk()).toBeTruthy();
 });
